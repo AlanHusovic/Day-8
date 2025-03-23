f0c602e2f5269657c6dd33e8fe1d324a5786311e
AlanHusovic <alanhusovic755@gmail.com>
2025-03-23 13:00:05 +0100

    Create 62.py

---
+++ 62.py
@@ -0,0 +1,13 @@
+# def great_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+
+# great_with_name("Alan")
+
+# Function with more than 1 inputs
+
+def great_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+great_with(name="Angela", location="London")
