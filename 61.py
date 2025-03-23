c043949fe4624e33b7d8961c17a36b007085e510
AlanHusovic <alanhusovic755@gmail.com>
2025-03-23 12:34:51 +0100

    Create 61.py

---
+++ 61.py
@@ -0,0 +1,12 @@
+def great():
+    print("Hello Alan")
+    print("How do you do Jack Bauer")
+    print("Isnt the weather nice?")
+
+# Functions that allows for inputs
+
+def great_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+great_with_name("Alan")
